dd37bc09645b29603ff7571f3cf58de9b113e75e
Dario <dario.gdasilva@hotmail.com>
2021-10-18 20:30:29 -0300

    rama1

---
+++ main.js
@@ -0,0 +1,21 @@
+let nombre = prompt('Ingrese su nombre');
+
+let pedirNumero = parseInt ( prompt("Ingresar numero") );
+
+let apellido = prompt("Ingrese su apellido");
+
+alert(nombre + " " + apellido + "!");
+
+const año = 2021;
+
+let fechaNacimiento =  parseInt( prompt("Ingresa fecha de nacimiento"))
+
+let Edad = (año - fechaNacimiento)
+
+document.write(Edad)
+
+console.log (nombre)
+console.log(apellido)
+console.log (fechaNacimiento)
+console.log (Edad)
+
