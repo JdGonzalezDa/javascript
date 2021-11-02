e83667c0f2a761e9463a218e1c3e370798b90ba3
JdGonzalezDa <89804667+JdGonzalezDa@users.noreply.github.com>
2021-11-01 22:53:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-function hacerAlgo(dato1,dato2) {
-    
-    document.write('<p style="color:white">${dato1} ${dato2}</p>');
- }
+let notaPrimerCuatrimestre = parseInt(prompt("Ingrese la primera nota"));
+let notaSegundoCuatrimestre = parseInt(prompt("Ingrese la segunda nota"));
+let notaTercerCuatrimestre = parseInt(prompt("Ingrese la tercera nota"));
+let notaFinal = parseInt("Ingrese la nota final: ")
 
-hacerAlgo();
-hacerAlgo("Hola");
-hacerAlgo("Hola", "Mundo");
-
+function calcular(notaPrimerCuatrimestre, notaSegundoCuatrimestre, notaSegundoCuatrimestre, notaFinal){
+    switch(notaFinal){
+        case "+":
+            return notaPrimerCuatrimestre + notaSegundoCuatrimestre + notaTercerCuatrimestre
+            break;
+        case "-":
+            return notaPrimerCuatrimestre - notaSegundoCuatrimestre - notaTercerCuatrimestre
+            break;
+            case "/":
+            return notaPrimerCuatrimestre / notaSegundoCuatrimestre / notaTercerCuatrimestre
+            break;
+            default:
+                return 0;
+                break;
+    }
+}
+let resultado = calcular(notaPrimerCuatrimestre, notaSegundoCuatrimestre, notaTercerCuatrimestre, notaFinal)
+alert(resultado)
