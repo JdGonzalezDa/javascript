e937feab8d04673c92c007901d6d0acc2214d8ba
JdGonzalezDa <89804667+JdGonzalezDa@users.noreply.github.com>
2021-11-01 23:46:34 -0300

    simulador

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-let notaPrimerCuatrimestre = parseInt(prompt("Ingrese la primera nota"));
-let notaSegundoCuatrimestre = parseInt(prompt("Ingrese la segunda nota"));
-let notaTercerCuatrimestre = parseInt(prompt("Ingrese la tercera nota"));
-let notaFinal = parseInt("Ingrese la nota final: ")
+alert(" Bienvenidos Les daremos sus notas finales")
 
-function calcular(notaPrimerCuatrimestre, notaSegundoCuatrimestre, notaSegundoCuatrimestre, notaFinal){
-    switch(notaFinal){
-        case "+":
-            return notaPrimerCuatrimestre + notaSegundoCuatrimestre + notaTercerCuatrimestre
-            break;
-        case "-":
-            return notaPrimerCuatrimestre - notaSegundoCuatrimestre - notaTercerCuatrimestre
-            break;
-            case "/":
-            return notaPrimerCuatrimestre / notaSegundoCuatrimestre / notaTercerCuatrimestre
-            break;
-            default:
-                return 0;
-                break;
-    }
+let nombre = prompt("Ingrese su nombre: ")
+let apellido = prompt("Ingrese su apellido: ")
+saludar(nombre, apellido)
+
+let edad = parseInt(prompt("Edad: "))
+while (edad < 18 ){
+    alert("El dato es incorrecto, ingrese nuevamente");
+    edad = parseInt(prompt("Edad: "))
 }
-let resultado = calcular(notaPrimerCuatrimestre, notaSegundoCuatrimestre, notaTercerCuatrimestre, notaFinal)
-alert(resultado)
+
+function saludar(nombre, apellido){ 
+    alert(`Bienvenido alumno : ${nombre}${apellido}`)
+}
+let primeraNota = parseInt(prompt("ingrese la primera nota: "))
+let segundaNota = parseInt(prompt("ingrese la segunda nota: "))
+let terceraNota = parseInt(prompt("ingrese la primera nota: "))
+
+let sumaNotas = (a, b, c) => a + b + c
+
+let promedio = function(primeraNota, segundaNota, terceraNota){return sumaNotas(primeraNota,segundaNota,terceraNota) / 3}
+alert("El promedio de notas es de " + promedio(primeraNota,segundaNota,terceraNota))
+
+if (promedio(primeraNota,sedundaNota,terceraNota)< 6){
+    alert("Desaprobaste el Año, Tenes que recursar la materia");
+    console.log("Desaprobaste el Año, Tenes que recursar la materia");
+    document.write("Desaprobaste el Año, Tenes que recursar la materia");
+}else{
+    alert("Felicitaciones Aprobaste el la materia");
+    console.log("Felicitaciones Aprobaste el la materia");
+    document.write("Felicitaciones Aprobaste el la materia");
+}
